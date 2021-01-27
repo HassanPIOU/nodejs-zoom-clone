9e070649c352a7396e9bea71dd09ecc16b08aac6
Hassan PIOU <piouhassan@gmail.com>
2021-01-27 10:20:31 +0000

    finished

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
-
 const path =  require('path')
 const {v4 : uuidv4 } = require('uuid')
-
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const {ExpressPeerServer} = require('peer')
+const peerServer = ExpressPeerServer(server,{
+    debug : true
+})
 
 
 //Middlewares
+app.use('/peerjs',peerServer)
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname,'./public')))
 
@@ -22,6 +27,26 @@ app.get('/:room',(req,res) => {
 })
 
 
-app.listen(3000,() => {
+
+// Socket io
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+        // messages
+        socket.on('message', (message) => {
+            //send message to the same room
+            io.to(roomId).emit('createMessage', message)
+        });
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        })
+    })
+})
+
+
+
+server.listen(3000,() => {
     console.log(`server is running `)
 })
